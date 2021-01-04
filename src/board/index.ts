73c972111e2b55e26a37a643db05523f103bd537
Mohsin Hussain <mohsinhussain1993@gmail.com>
2021-01-05 02:18:18 +0500

    added x and y

--- src/board/index.ts
+++ src/board/index.ts
@@ -68,15 +68,23 @@ export class Board extends BoardBase {
     let currentY = 0
     const doAnimation = async () => {
       clearCanvas(this)
-      drawCircle(this.context, {
-        "x":3 * BoardBase.PIECE_RADIUS * x + BoardBase.MASK_X_BEGIN + 2 * BoardBase.PIECE_RADIUS,
-        "y":3 * BoardBase.PIECE_RADIUS * y + BoardBase.MASK_Y_BEGIN + 2 * BoardBase.PIECE_RADIUS,
-        "r": BoardBase.PIECE_RADIUS,
-       // "player": this.getPlayer(this.map[y][x]),
-      })
-      this.render()
-      currentY += BoardBase.PIECE_RADIUS
+      
+      for (let y = 0; y < BoardBase.ROWS; y++) {
+        for (let x = 0; x < BoardBase.COLUMNS; x++) {
+          drawCircle(this.context, {
+            "x":3 * BoardBase.PIECE_RADIUS * x + BoardBase.MASK_X_BEGIN + 2 * BoardBase.PIECE_RADIUS,
+            "y":3 * BoardBase.PIECE_RADIUS * y + BoardBase.MASK_Y_BEGIN + 2 * BoardBase.PIECE_RADIUS,
+            "r": BoardBase.PIECE_RADIUS,
+          // "player": this.getPlayer(this.map[y][x]),
+          })
+          this.render()
+          currentY += BoardBase.PIECE_RADIUS
+     
     }
+    }
+    }
+
+
     while (newRow * 3 * BoardBase.PIECE_RADIUS >= currentY) {
       await animationFrame()
       doAnimation()
